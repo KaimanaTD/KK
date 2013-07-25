1203894ae1740d56304d7a7cb3f41e75d962666b
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2013-07-25 11:53:47 -1000

    Begin template structure.
    Break out H5BP index.html into chunks that can be included at will.
    Broken out:
    1. head - meta tags and stylehseet calls
    2. oldBrowser - conditional HTML that prompts a browser upgrade
    3. scripts - all script calls are placed at the end of <body>.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+  <?php require(partial/head.php); ?>
+</head>
+<body>
+  <?php include(partial/oldBrowser.php); ?>
+  <!-- Add your site or application content here -->
+
+  <?php require(partial/scripts.php); ?>
+</body>
+</html>
