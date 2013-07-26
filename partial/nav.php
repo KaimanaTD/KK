e2344aafda8c7d44bab78fc00f37efee384c05cf
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2013-07-25 21:30:28 -1000

    Start boilerplate for nav bar.
    Elected to go with Tim Pietrusky's "Select" schema:
    http://css-tricks.com/responsive-menu-concepts

---
+++ partial/nav.php
@@ -0,0 +1,23 @@
+<?php 
+  $pagenames = array(
+    array("Home","#"),
+    array("Bids and Fess","bidding.php"),
+    array("Logistics","logistics.php"),
+    array("Teams","teams.php"),
+    array("Hat Draw","hatdraw.php"),
+    array("History","history.php"),
+    array("Contact","contacts.php")
+  );
+?>
+<nav>
+  <ul> 
+    <?php foreach ($pagenames as $page) { ?>
+      <li><a href="<?php echo $page[1];?>"><?php echo $page[0];?></a></li>
+    <?php } ?>
+  </ul>
+  <select onchange="if (this.value) window.location.href = this.value;">
+    <?php foreach ($pagenames as $page) { ?>
+      <option value="<?php echo $page[1];?>"><?php echo $page[0];?></option>
+    <?php } ?>
+  </select>
+</nav>
