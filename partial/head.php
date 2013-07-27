5002ea98cbd7773bc48bb3f2abd2e8bb078d6ee8
Clay McKell on Bellman <kcmckell@gmail.com>
2013-07-26 15:43:23 -1000

    Move analytics to HEAD.
    To capture all pageviews, move analytics script to HEAD.  This will
    capture views even if they click away before the scripts are loaded at
    the bottom of BODY.

--- partial/head.php
+++ partial/head.php
@@ -10,3 +10,13 @@
 <link rel="stylesheet" href="css/main.css">
 <link rel="stylesheet" href="css/nav.css">
 <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+    e.src='//www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    ga('create','UA-XXXXX-X');ga('send','pageview');
+</script>
