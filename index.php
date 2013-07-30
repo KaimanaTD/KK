b152d8e8083742572821b818cdfa07da24304523
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2013-07-29 14:21:35 -1000

    Add testimonials to index.

--- index.php
+++ index.php
@@ -31,7 +31,57 @@
         Sample Text.
       </p>
 	</aside>
-  </div>
+    <section class="testimonials grid-100 mobile-grid-100 grid-parent">
+      <div class="testimonial grid-50 mobile-grid-50">
+        <blockquote>
+          I've been playing ultimate with silly dedication for 7 years, and my first trip to Kaimana this past weekend was hands-down the most fun I have ever had at a tournament.
+        </blockquote>
+        <cite>Sam O'Brien</cite>
+      </div>
+      <div class="testimonial grid-50 mobile-grid-50">
+        <blockquote>
+          As always, it was the best.  This was #6 for me, but I hadn't been back since 2000, now I'm thinking, "How can I stay away??"!!  The lined fields, the score boards, the abundant food -- all great.
+        </blockquote>
+        <cite>Joanne Adamkewicz</cite>
+      </div>
+      <div class="testimonial grid-50 mobile-grid-50">
+        <blockquote>
+          This has got to be one of the best tournaments I've ever been to!  I found spirit to be the main theme of the weekend.  Everyone is there to play hard ultimate during the day, but when the day is done, it's a big social event.  It is very easy to make new friends in this environment.
+        </blockquote>
+        <cite>Christopher Yap</cite>
+      </div>
+      <div class="testimonial grid-50 mobile-grid-50">
+        <blockquote>
+          Having been to over a dozen Kaimana Klassiks, I have to say that it is the best ultimate experience I've ever had.  Where else can you enjoy the peaceful, relaxing climate, the aloha of the local community, and have fun playing ultimate all at the same time?
+        </blockquote>
+        <cite>JD Lobue, Jr.</cite>
+      </div>
+      <div class="testimonial grid-50 mobile-grid-50">
+        <blockquote>
+          It really is the best tournament in the U.S., and rivals only Paganello as the best tournament in the world.  I hear next year's 20th annual tourney is one not to be missed, so I'll see you there.
+        </blockquote>
+        <cite>Jill Van Wie</cite>
+      </div>
+      <div class="testimonial grid-50 mobile-grid-50">
+        <blockquote>
+          I was there with the womens Bec team, from Montreal, that won the spirit award, you the chicks that went topless!  Just wanted to say that I agree, best tournament I've ever been to!
+        </blockquote>
+        <cite>Sparta</cite>
+      </div>
+      <div class="testimonial grid-50 mobile-grid-50">
+        <blockquote>
+          I can honestly say that Kaimana Klassik changed my life!
+        </blockquote>
+        <cite>Kenny Gonzales</cite>
+      </div>
+      <div class="testimonial grid-50 mobile-grid-50">
+        <blockquote>
+          Thank you for the hospitality and the greatest tournaments.  I am glad for new teammates and friends, having found a state I should have found a long time ago and enjoyed it through great hosts and two spectacular tournaments.
+        </blockquote>
+        <cite>Jim Siewert</cite>
+      </div>
+    </section>
+  </div><!-- /content -->
   <?php include('partial/foot.php'); ?>
   </div> <!-- /wrapper -->
   <?php require('partial/scripts.php'); ?>
