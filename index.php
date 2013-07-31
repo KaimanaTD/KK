243ca82cc4526c2efef7ffbc60d4a50bdff3c77f
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2013-07-30 16:51:07 -1000

    Reorganize testimonial section.
    Incorporate avatar placeholders.
    Rework layout and styling.

--- index.php
+++ index.php
@@ -33,53 +33,77 @@
       <a class="twitter-timeline" data-dnt="true" data-tweet-limit="5" data-chrome="" href="https://twitter.com/search?q=%40KaimanaKlassik+OR+%23KK27" data-widget-id="360230608397541376">Tweets about "@KaimanaKlassik OR #KK27"</a>
 	</aside>
     <section class="testimonials grid-100 mobile-grid-100 grid-parent">
-      <div class="testimonial grid-50 mobile-grid-50">
-        <blockquote>
+      <div class="testimonial grid-50 mobile-grid-50 grid-parent">
+        <div class="testifier grid-25 mobile-grid-100 mobile-push-100">
+          <img class="avatar" style="max-width:100%" src="img/PersonIcon.png" />
+          Sam O'Brien
+        </div>
+        <div class="quotation grid-75 mobile-grid-100 mobile-pull-100">
           I've been playing ultimate with silly dedication for 7 years, and my first trip to Kaimana this past weekend was hands-down the most fun I have ever had at a tournament.
-        </blockquote>
-        <cite>Sam O'Brien</cite>
+        </div>
       </div>
-      <div class="testimonial grid-50 mobile-grid-50">
-        <blockquote>
+      <div class="testimonial grid-50 mobile-grid-50 grid-parent">
+        <div class="testifier grid-25 mobile-grid-100 mobile-push-100">
+          <img class="avatar" style="max-width:100%" src="img/PersonIcon.png" />
+          Joanne Adamkewicz
+        </div>
+        <div class="quotation grid-75 mobile-grid-100 mobile-pull-100">
           As always, it was the best.  This was #6 for me, but I hadn't been back since 2000, now I'm thinking, "How can I stay away??"!!  The lined fields, the score boards, the abundant food -- all great.
-        </blockquote>
-        <cite>Joanne Adamkewicz</cite>
+        </div>
       </div>
-      <div class="testimonial grid-50 mobile-grid-50">
-        <blockquote>
+      <div class="testimonial grid-50 mobile-grid-50 grid-parent">
+        <div class="testifier grid-25 mobile-grid-100 mobile-push-100">
+          <img class="avatar" style="max-width:100%" src="img/PersonIcon.png" />
+          Christopher Yap
+        </div>
+        <div class="quotation grid-75 mobile-grid-100 mobile-pull-100">
           This has got to be one of the best tournaments I've ever been to!  I found spirit to be the main theme of the weekend.  Everyone is there to play hard ultimate during the day, but when the day is done, it's a big social event.  It is very easy to make new friends in this environment.
-        </blockquote>
-        <cite>Christopher Yap</cite>
+        </div>
       </div>
-      <div class="testimonial grid-50 mobile-grid-50">
-        <blockquote>
+      <div class="testimonial grid-50 mobile-grid-50 grid-parent">
+        <div class="testifier grid-25 mobile-grid-100 mobile-push-100">
+          <img class="avatar" style="max-width:100%" src="img/PersonIcon.png" />
+          JD Lobue, Jr.
+        </div>
+        <div class="quotation grid-75 mobile-grid-100 mobile-pull-100">
           Having been to over a dozen Kaimana Klassiks, I have to say that it is the best ultimate experience I've ever had.  Where else can you enjoy the peaceful, relaxing climate, the aloha of the local community, and have fun playing ultimate all at the same time?
-        </blockquote>
-        <cite>JD Lobue, Jr.</cite>
+        </div>
       </div>
-      <div class="testimonial grid-50 mobile-grid-50">
-        <blockquote>
+      <div class="testimonial grid-50 mobile-grid-50 grid-parent">
+        <div class="testifier grid-25 mobile-grid-100 mobile-push-100">
+          <img class="avatar" style="max-width:100%" src="img/PersonIcon.png" />
+          Jill Van Wie
+        </div>
+        <div class="quotation grid-75 mobile-grid-100 mobile-pull-100">
           It really is the best tournament in the U.S., and rivals only Paganello as the best tournament in the world.  I hear next year's 20th annual tourney is one not to be missed, so I'll see you there.
-        </blockquote>
-        <cite>Jill Van Wie</cite>
+        </div>
       </div>
-      <div class="testimonial grid-50 mobile-grid-50">
-        <blockquote>
+      <div class="testimonial grid-50 mobile-grid-50 grid-parent">
+        <div class="testifier grid-25 mobile-grid-100 mobile-push-100">
+          <img class="avatar" style="max-width:100%" src="img/PersonIcon.png" />
+          Sparta
+        </div>
+        <div class="quotation grid-75 mobile-grid-100 mobile-pull-100">
           I was there with the womens Bec team, from Montreal, that won the spirit award, you the chicks that went topless!  Just wanted to say that I agree, best tournament I've ever been to!
-        </blockquote>
-        <cite>Sparta</cite>
+        </div>
       </div>
-      <div class="testimonial grid-50 mobile-grid-50">
-        <blockquote>
+      <div class="testimonial grid-50 mobile-grid-50 grid-parent">
+        <div class="testifier grid-25 mobile-grid-100 mobile-push-100">
+          <img class="avatar" style="max-width:100%" src="img/PersonIcon.png" />
+          Kenny Gonzales
+        </div>
+        <div class="quotation grid-75 mobile-grid-100 mobile-pull-100">
           I can honestly say that Kaimana Klassik changed my life!
-        </blockquote>
-        <cite>Kenny Gonzales</cite>
+        </div>
       </div>
-      <div class="testimonial grid-50 mobile-grid-50">
-        <blockquote>
+      <div class="testimonial grid-50 mobile-grid-50 grid-parent">
+        <div class="testifier grid-25 mobile-grid-100 mobile-push-100">
+          <img class="avatar" style="max-width:100%" src="img/PersonIcon.png" />
+          Jim Siewert
+        </div>
+        <div class="quotation grid-75 mobile-grid-100 mobile-pull-100">
           Thank you for the hospitality and the greatest tournaments.  I am glad for new teammates and friends, having found a state I should have found a long time ago and enjoyed it through great hosts and two spectacular tournaments.
-        </blockquote>
-        <cite>Jim Siewert</cite>
+        </div>
       </div>
     </section>
   </div><!-- /content -->
