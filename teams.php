b5898f62b5f64445f432908f3f383bc7ffb0a861
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2013-07-30 17:05:59 -1000

    Build out pages.
    Build out main level pages based on page_template.

---
+++ teams.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+  <?php require('partial/head.php'); ?>
+  <title></title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+</head>
+<body>
+  <?php include('partial/oldBrowser.php'); ?>
+  <div class="wrapper grid-parent">
+  <header>
+    <?php include('partial/banner.php'); ?>
+    <?php require('partial/nav.php'); ?>
+  </header>
+  <!-- Add your site or application content here -->
+  <div class="content">
+	<section class="grid-70">
+      <article>
+        <h1>Article Title</h1>
+        <p>
+          Body text.
+        </p>
+      </article>
+	</section>
+    <aside class="grid-30">
+      <p>
+        Sample Text.
+      </p>
+	</aside>
+  </div>
+  <?php include('partial/foot.php'); ?>
+  </div> <!-- /wrapper -->
+  <?php require('partial/scripts.php'); ?>
+</body>
+</html>
