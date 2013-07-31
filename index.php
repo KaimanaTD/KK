6536e4b95bf0d75e9e17dbdf644cb7ef22243180
jk1ine <kline.jena@gmail.com>
2013-07-30 15:17:40 -1000

    Update index.php
    So, I'm messing around with editing the text on the home page, but am not sure about headers, grid sizes, etc.  I guess I just need to know if I should be changing the <h1>, etc things or just entering the text I want somewhere.  I started to edit header stuff a bit, but don't want to mess it up so stopped and just entered text (not even sure if it is entered in the right area ~ there was a "main" and an "article" section and I am not sure which I should be working in).  Sorry for the questions/confusion, but I think I will figure it out soon with a bit more practice.  Also, I am not sure where the headers (tabs?) are.  I know I saw them on the mock up site you showed me (Home, Bids&Fees, Logistics, ..)

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
   <?php require('partial/head.php'); ?>
-  <title></title>
+  <title> Kaimana Klassik </title>
   <meta name="description" content="">
-  <meta name="keywords" content="">
+  <meta name="keywords" content="Hawaii, ultimate, frisbee, ultimate frisbee, Waimanalo, tournament>
 </head>
 <body>
   <?php include('partial/oldBrowser.php'); ?>
@@ -16,22 +16,54 @@
     <?php include('partial/banner.php'); ?>
     <?php require('partial/nav.php'); ?>
   </header>
+  
   <!-- Add your site or application content here -->
   <div class="content">
     <section class="main grid-70">
       <article>
-        <h1>Sample Article Title</h1>
-        <p>
-          Body text.
-        </p>
-      </article>
+        <h1>Greetings from Kaimana!</h1>
+        <p><center><strong>HAWAII'S BIGGEST AND BEST WOMENS AND OPEN TOURNAMENT</strong></p>
+	<p>16 Open and 16 Women's Teams</p>
+	<hr><p>The party starts on Friday, February 14 with evening registration and entertainment, and play continues 		
+	through the President’s Day holiday on Monday, February 17. </p>
+	<p>Make your trip a real vacation and stay through the following weekend, where you can join us for the Post-Kaimana Hilo Hat Draw!  Stay tuned for details.  </p>
+
+  <hr>
+		
+<b>Start the Chatter! </b>
+	<p><b>Team bids are currently being accepted</b></p>
+
+<!-- Here is where I quit trying to edit formatting -->
+	$420 team deposit due on NOVEMBER 8, 2013
+	Send in your bids now.
+	How to submit a team bid:
+	
+        Bids can be in any format you wish, but must include:
+        
+        Contact information for captain or organizer, including name, email address, and phone number(s);
+        The number of players you actually anticipate having on your roster come February, as well as the number you have that are currently committed 
+        $420 deposit (3 player fees); U.S. dollars in the form of a check payable to Kaimana Klassik, money order, or pay online now.
+        
+        Preference will be given to bids that also include: 
+        
+        What your team can contribute to Kaimana 25 both competitively and in spirit, 
+        Your team's history at Kaimana, if any, or your team's history in general,
+        Something fun, original, and convincing!
+        
+        Send all team bids to:  Jack Wade, 1308 Center Street, Apartment B2, Honolulu HI 96816.
+        
+
+NO TEAM? NO PROBLEM!
+Individuals will be placed on the Aloha Spirit team or on other teams needing players.
+
+       </article>
 	</section><!-- /main -->
     <aside class="grid-30">
       <p>
         Sample Text.
       </p>
       <a class="twitter-timeline" data-dnt="true" data-tweet-limit="5" data-chrome="" href="https://twitter.com/search?q=%40KaimanaKlassik+OR+%23KK27" data-widget-id="360230608397541376">Tweets about "@KaimanaKlassik OR #KK27"</a>
-	</aside>
+	</asie>
     <section class="testimonials grid-100 mobile-grid-100 grid-parent">
       <div class="testimonial grid-50 mobile-grid-50">
         <blockquote>
