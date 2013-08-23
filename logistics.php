04ae62d9e6fd18b6b247bcf914183c5a048a37a3
Clay McKell on Bellman <kcmckell@gmail.com>
2013-08-22 14:13:22 -1000

    Clean up Logistics.
    Clean up markup.
    Edit copy on bus rides.

--- logistics.php
+++ logistics.php
@@ -18,71 +18,105 @@
   </header>
   <!-- Add your site or application content here -->
   <div class="content">
-	<section class="grid-70">
-      <article>
-        <h1>Logistics</h1>
-        <p>
-          <p><strong>Who?</strong></p>
-		<ul>
-		  <li>Up to 16 Open teams and 16 Women's teams. In past years, teams have hailed from Asia to Australia to Europe to mainland U.S. to just down the street. Check back in November for specific teams. </li>
-	    </ul>
-		<p><strong>What?</strong></p>
-		<ul>
-	      <li> Food. Fun. Camping. Dancing. Swimming. AND ULTIMATE!
-          </ul>		
-		<p><strong>When?</strong></p>
-		<ul>
-	      <li> February 15, 16, 17, 2014. Important days/times to note:
-            <ul>
-              <li>Friday, February 14, 5:00 to 9:00 pm  player check in. Dinner is served from 6:00-8:00 pm.
-              <li>Friday, February 14, 8:00 pm  Captains meeting
-              <li>Saturday, February 15, Breakfast starts at 7:00 am, Player check-in from 7:00 to 7:50 am, 8:00 am  Opening Ceremony, 
-              <li>Saturday, February 15, Games from 8:45 am to 5:30 pm, Dinner at 6:00 pm.
-              <li>Sunday, February 16, Breakfast starts at 7:00 am, Games from 8:45 am to 5:30 pm, Dinner at 6:00.
-              <li>Monday, February 17, Breakfast starts at 7:00 am, Bracket play starts at 8:45 am, Finals ~12:30 and 3:00 pm followed by Closing Ceremony at ~5:00. Don't schedule your flight too early or you'll miss them! </li>
-              <li>Saturday-Sunday, February 22-23-Post-Kaimana Hat Draw, Hilo</a> - Stay around for some interisland fun!</li>      
-    </ul>
-        </ul>		
-		<p><strong>Where?</strong></p>
-		<ul>
-          <li>Waimanalo Polo Fields, 41-1200 Kalanianaole Highway, Waimanalo, Oahu <br>
-              <a href="maps.html"><font color="#FF00BF">Click here for Oahu and Waimanalo maps</font></a><font color="#FF00BF">.</font> <br>
-          <li>Where do I stay?
-              <ul>
-                <li>Free camping is provided just across the streets from the fields at Waimanalo Bay Beach Park for Friday through Monday evenings. By state law, all campsites are closed on Wednesday and Thursday nights. There is no camping directly on the beach. Campsites have running water, restrooms, and cold showers. Check back closer to the tournament for more detailed camping information. </li>
-              </ul>
-          </li>
-		</ul>		
-		<ul>
-          <li>How do I get there?
-              <ul>
-                <li>By Plane:&nbsp; Fly into Honolulu International Airport (HNL). Just about every major airline serves HNL. </li>
-              </ul>
-          </li>
-	    </ul>
-		<ul>
-          <ul>
-            <li>From the airport, Waikiki, or anywhere in Honoluluget on H1 East. Take it until it ends! H1 turns into Kalanianaole Highway, which passes by Hawaii Kai, Hanauma Bay, Sandy Beach, Sea Life Park, and through Waimanalo. When you see the McDonald's on your right, in Waimanalo, you've got less than a half-mile to go. The fields will be on your left; camping is at Waimanalo Bay Beach Park, on your right. </li>
-          </ul>
-	    </ul>
-		<ul>
-          <ul>
-            <li>From the airportget on bus 19 or 20 heading towards Waikiki Beach. Get off at Hotel and Alakea. Cross Hotel Street, walk up Alakea a few meters (by the District Court House) and transfer to bus 57 heading towards Kailua/Sea Life Park. The bus ride is 30 minutes to an hour after you transfer, so be patient! Get off at the bus stop right outside of the Waimanalo Polo Fields/ Waimanalo Beach Recreation Area. Officially, no luggage is allowed on the bus, so pack light! From Waikikicatch bus 58 heading towards Hawaii Kai/Sea Life Park from any of a number of bus stops on Kuhio Avenue. After a long (45 min or so) ride, get off in Waimanalo at the bus stop right outside of the Waimanalo Polo Fields/ Waimanalo Beach Recreation Area. Bus rides are $2 one way. </li>
-          </ul>
-	    </ul>
-		<p><strong>Why?</strong></p>
-		<ul>
-		  <li> Cause it's the most fun you've ever had at a tournament.
-</ul>
-
-        </p>
-      </article>
+	<section class="grid-100">
+		<h1>Logistics</h1>
+		<dl>
+				<dt><h2>Who?</h2></dt>
+				<dd>
+						Up to 16 Open teams and 16 Women's teams. 
+						In past years, teams have hailed from Asia to Australia to Europe to mainland U.S. to just down the street. 
+						Check back in November for specific teams.
+				</dd>
+				<dt><h2>What?</h2></dt>
+				<dd> Food. Fun. Camping. Dancing. Swimming. AND ULTIMATE!</dd>
+				<dt><h2>When?</h2></dt>
+				<dd>
+						February 15, 16, 17, 2014. Important days/times to note:
+						<dl>
+								<dt>Friday, February 14</dt>
+								<dd><ul>
+										<li>5:00 to 9:00 pm - Player check-in</li>
+										<li>6:00 to 8:00 pm - Dinner is served</li>
+										<li>8:00 pm - Captains meeting</li>
+								</ul></dd>
+								<dt>Saturday, February 15</dt>
+								<dd><ul>
+										<li>7:00 am - Breakfast starts</li>
+										<li>7:00 to 7:50 am - Player check-in</li>
+										<li>8:00 am - Opening Ceremony</li>
+										<li>8:45 am to 5:30 pm - Games</li>
+										<li>6:00 pm - Dinner</li>
+								</ul></dd>
+								<dt>Sunday, February 16</dt>
+								<dd><ul>
+										<li>7:00 am - Breakfast starts</li>
+										<li>8:45 am to 5:30 pm - Games</li>
+										<li>6:00 pm - Dinner</li>
+								</ul></dd>
+								<dt>Monday, February 17</dt>
+								<dd>
+										<ul>
+												<li>7:00 am - Breakfast starts</li>
+												<li>8:45 am - Bracket play starts</li>
+												<li>~12:30 and 3:00 pm - Finals</li>
+												<li>~5:00 - Closing Ceremony (immediately follows Finals)</li>
+										</ul>
+										<p>
+												Don't schedule your flight too early or you'll miss the Finals and the Closing Ceremony.
+										</p>
+								</dd>
+								<dt>Saturday-Sunday, February 22-23</dt>
+								<dd>Post-Kaimana Hat Draw, Hilo.  Stay around for some interisland fun!</dd>
+						</dl>
+				<dt><h2>Where?</h2></dt>
+				<dd>
+						Waimanalo Polo Fields, 41-1200 Kalanianaole Highway, Waimanalo, Oahu.  For your reference we have <a href="maps.php">maps of Oahu and Waimanalo</a>.
+						<dl>
+								<dt>Where do I stay?</dt>
+								<dd>
+										Free camping is provided just across the streets from the fields at Waimanalo Bay Beach Park for Friday through Monday evenings. 
+										By state law, all campsites are closed on Wednesday and Thursday nights. 
+										There is no camping directly on the beach. 
+										Campsites have running water, restrooms, and cold showers. 
+										Check back closer to the tournament for more detailed camping information.
+								</dd>
+								<dt>How do I get there?</dt>
+								<dd><ul>
+										<li>
+												By Plane: Fly into Honolulu International Airport (HNL). 
+												Just about every major airline serves HNL. 
+										</li>
+										<li>
+												By car from the airport, Waikiki, or anywhere in Honolulu: Get on H1 East. 
+												Take it until it ends! 
+												H1 turns into Kalanianaole Highway, which passes by Hawaii Kai, Hanauma Bay, Sandy Beach, Sea Life Park, and through Waimanalo. 
+												When you see the McDonald's on your right, in Waimanalo, you've got less than a half-mile to go. 
+												The fields will be on your left; camping is at Waimanalo Bay Beach Park, on your right. 
+										</li>
+										<li>
+												By bus from the airport: Get on bus 19 or 20 heading towards Waikiki Beach. 
+												Get off at Hotel and Alakea. 
+												Cross Hotel Street, walk up Alakea a few meters (by the District Court House) and transfer to bus 57 heading towards Kailua/Sea Life Park. 
+												The bus ride is 30 minutes to an hour after you transfer, so be patient! 
+												Get off at the bus stop right outside of the Waimanalo Polo Fields/Waimanalo Beach Recreation Area. 
+										</li>
+										<li>
+												By bus from Waikiki: Catch bus 58 heading towards Hawaii Kai/Sea Life Park from any of a number of bus stops on Kuhio Avenue. 
+												After a long (45 min or so) ride, get off in Waimanalo at the bus stop right outside of the Waimanalo Polo Fields/Waimanalo Beach Recreation Area. 
+										</li>
+										<li>
+												General bus notes: Bus fares are $2.50 and include one transfer within an hour of your first boarding.
+												Officially, no luggage is allowed on the bus, so pack light!
+												This rule is enforced sporadically, but if you can fit all your bags on your lap, you should be OK.
+												Real-time transit info can be found via <a href="http://hea.thebus.org/">The Bus</a> or <a href="http://www.google.com/transit">Google Transit</a>.
+										</li>
+								</ul></dd>
+						</dl>
+				</dd>
+				<dt><h2>Why?</h2></dt>
+				<dd>'Cause it's the most fun you've ever had at a tournament.</dd>
+		</dl>
 	</section>
-    <aside class="grid-30">
-      <p>
-        Sample Text.
-      </p>
-	</aside>
   </div>
   <?php include('partial/foot.php'); ?>
   </div> <!-- /wrapper -->
