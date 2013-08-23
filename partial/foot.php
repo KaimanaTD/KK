fdf1d39994ba6a3d1f9f889930f51187d4be0a21
Clay McKell on Bellman <kcmckell@gmail.com>
2013-08-22 16:54:45 -1000

    Remove dummy links from Footer.
    There's no real need for web design credits.
    There's also no real need for a privacy policy.  The sensitive
    information is collected via a 3rd party.
    Closes Issue #9

--- partial/foot.php
+++ partial/foot.php
@@ -1,10 +1,4 @@
 <footer class="grid-100 mobile-grid-100 grid-parent">
-	<div class="grid-50 mobile-grid-50">
-		Web Design
-	</div>
-	<div class="grid-50 mobile-grid-50">
-		Privacy Policy
-	</div>
 	<div class="grid-100">
 		Kaimana Klassik is sponsored by HULA, the Hawaii Ulimate League Association.  Questions?  Email the <a href="mailto:kaimanatd@gmail.com">Tournament Directors: kaimanatd (at) gmail (dot) com</a>.
 	</div>
