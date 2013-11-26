894b8d7826effe17305e9fa2307c0f64c93c4a51
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2013-11-25 15:55:59 -1000

    Convert payment JS to use real data.
    Implement the three databases (one for each division: Open, Women, Guest) in an array that is AJAX'ed into and then each in turn is inserted into the 'teamselect' variable.
    
    Also clean up player selection.

--- playerpayment.php
+++ playerpayment.php
@@ -80,74 +80,67 @@
   <script src="js/main.js"></script>
   <script>
   jQuery(document).ready(function(){
-  // Registration fanciness
-  // Nifty Ajax methodology:
-  // http://stackoverflow.com/questions/8840257/jquery-ajax-handling-continue-responses-success-vs-done
-  var $regform = $('form#registration');
-  var prices = {'player_early': 140, 'guest': 60};
-  prices['player'] = prices.player_early;
-  if ($regform) {
-    var $add_team_button = $regform.find('button#add_team');
-    var $firstfs = build_team_list(1);
-    $regform.prepend($firstfs);
-    $add_team_button.click(function(){
-      var $all_fs = $regform.find('fieldset');
-      var n = $all_fs.length;
-      var $next_fs = build_team_list(n+1);
-      $all_fs.after($next_fs);
-    });
-    $regform.on('submit',function(e){
-      e.preventDefault();
-      var form_vals = $(this).serializeArray();      
-      var players = [];
-      var guests = [];
-      var subtotal = 0;
-      var all_ids = [];
-      $.each(form_vals.filter(function(el){return el['name'].match(/^players.*$/gi);}), function(ind,val){
-        console.log(val);
-        // Should be in the form [ ID, First, Last ]
-        var item = val.value.split(',');
-        if (item[0][2] == 2) {
-          subtotal += prices.guest;
-          all_ids.push(item[0]);
-          guests.push('<tr><td>'+item.slice(1).join()+'</td><td>'+prices.guest+'</td></tr>');
+    // Registration fanciness
+    // Nifty Ajax methodology:
+    // http://stackoverflow.com/questions/8840257/jquery-ajax-handling-continue-responses-success-vs-done
+    var $regform = $('form#registration');
+    var prices = {'player_early': 140, 'guest': 80};
+    prices['player'] = prices.player_early;
+    if ($regform) {
+      var $add_team_button = $regform.find('button#add_team');
+      var $firstfs = build_team_list(1);
+      $regform.prepend($firstfs);
+      $add_team_button.click(function(){
+        var $all_fs = $regform.find('fieldset');
+        var n = $all_fs.length;
+        var $next_fs = build_team_list(n+1);
+        $all_fs.after($next_fs);
+      });
+      $regform.on('submit',function(e){
+        e.preventDefault();
+        var form_vals = $(this).serializeArray();      
+        var players = [];
+        var guests = [];
+        var subtotal = 0;
+        var all_ids = [];
+        $.each(form_vals.filter(function(el){return el['name'].match(/^players.*$/gi);}), function(ind,val){
+          // Should be in the form [ ID, First, Last ]
+          var item = val.value.split(',');
+          if (item[0][2] == 2) {
+            subtotal += prices.guest;
+            all_ids.push(item[0]);
+            guests.push('<tr><td>'+item.slice(1).join()+'</td><td>'+prices.guest+'</td></tr>');
+          } else {
+            subtotal += prices.player;
+            all_ids.push(item[0]);
+            players.push('<tr><td>'+item.slice(1).join()+'</td><td>'+prices.player+'</td></tr>');
+          };
+        });
+        var table = '<tr><th>Name</th><th>Price</th></tr>';
+        if (players.length > 0) {
+          table += '<tr><td>Players</td><td></td></tr>';
+          for (var p = 0; p < players.length; p++) {
+            table += players[p];
+          };
+        };
+        if (guests.length > 0) {
+          table += '<tr><td>Guests</td><td></td></tr>';
+          for (var g = 0; g < guests.length; g++) {
+            table += guests[g];
+          };
+        };
+        table += '<tr><td>Total:</td><td>'+subtotal+'</td>';
+        var $table = $('div#payment_summary table').html(table);
+        var $paypal_button = $('form.paypal-button');
+        var $amount = $paypal_button.find('input[name="amount"]').attr({"value":subtotal});
+        var $custom = $paypal_button.find('input[name="custom"]');
+        if ($custom.length > 0) {
+          $custom.attr({"value":all_ids.join(',')});
         } else {
-          subtotal += prices.player;
-          all_ids.push(item[0]);
-          players.push('<tr><td>'+item.slice(1).join()+'</td><td>'+prices.player+'</td></tr>');
+          $paypal_button.prepend('<input type="hidden" name="custom" value="'+all_ids.join(',')+'">');
         };
       });
-      console.log(players);
-      console.log(guests);
-      var table = '<tr><th>Name</th><th>Price</th></tr>';
-      if (players.length > 0) {
-        table += '<tr><td>Players</td><td></td></tr>';
-        for (var p = 0; p < players.length; p++) {
-          table += players[p];
-        };
-      };
-      if (guests.length > 0) {
-        table += '<tr><td>Guests</td><td></td></tr>';
-        for (var g = 0; g < guests.length; g++) {
-          table += guests[g];
-        };
-      };
-      table += '<tr><td>Total:</td><td>'+subtotal+'</td>';
-      var $table = $('div#payment_summary table').html(table);
-      var $paypal_button = $('form.paypal-button');
-      var $amount = $paypal_button.find('input[name="amount"]').attr({"value":subtotal});
-      var $custom = $paypal_button.find('input[name="custom"]');
-      if ($custom.length > 0) {
-        $custom.attr({"value":all_ids.join(',')});
-      } else {
-        $paypal_button.prepend('<input type="hidden" name="custom" value="'+all_ids.join(',')+'">');
-      };
-      // PSEUDOCODE:
-//      var $paypalbutton; 
-//      $paypalbutton.attr({'price': price, 'item': all_ids.join(',')});
-//      $paypalbutton.show;
-    });
-  }; // endif $regform
+    }; // endif $regform
   });
 
 function reg_get(url) {
@@ -163,9 +156,16 @@ function reg_get(url) {
 };
 
 function build_team_list(n) {
-  var reg_url = "http://spreadsheets.google.com/feeds/list/0ApzvRgA17RKMdFpyNUh0eWJKWVBuRmJlSkh5TGpNeWc/"
+  //var reg_url = "http://spreadsheets.google.com/feeds/list/0ApzvRgA17RKMdFpyNUh0eWJKWVBuRmJlSkh5TGpNeWc/";
+  var div_order = ["Open","Women","Guests"];
+  var reg_url_open = "http://spreadsheets.google.com/feeds/list/0ApzvRgA17RKMdGJsaVZHTjZsb204bUJRQUtCcS1Id1E/";
+  var reg_url_women = "http://spreadsheets.google.com/feeds/list/0ApzvRgA17RKMdGxRdmpRcUxnNmZuRS1adkFYTmZUTkE/";
+  var reg_url_guests = "http://spreadsheets.google.com/feeds/list/0ApzvRgA17RKMdEktVnhnczRJWEJOUzd3Wkd2dmR1cUE/";
+  var reg_url_array = [reg_url_open, reg_url_women, reg_url_guests];
   var format_suffix = '/public/values?alt=json';
-  var team_url = reg_url+"1"+format_suffix;
+  //var team_url = reg_url+"1"+format_suffix;
+  var team_request_array = [];
+  for (var div=0; div<reg_url_array.length; div++) {team_request_array.push(reg_get(reg_url_array[div]+"1"+format_suffix));};
   var teamdic = {
     'playerid':'gsx$_cre1l',
     'firstname':'gsx$_cn6ca',
@@ -173,26 +173,38 @@ function build_team_list(n) {
     'team':'gsx$_cpzh4'
   };
   var $fieldset = $('<fieldset/>');
-  reg_get(team_url).done(function(data, textStatus, jqXHR){
-    console.log(data);
+  // Multiple async calls handled via method here:
+  // http://stackoverflow.com/questions/9865586/jquery-when-troubleshooting-with-variable-number-of-arguments
+  var defer = $.when.apply($, team_request_array);
+  defer.done(function(){
     var $teamselect = $('<select/>', {id: 'team'+n, name: 'team'+n})
-      .append('<option value="" selected="selected">Please select a team</option>');
-    var data_array = data.feed.entry;
-    var team_info = [];
-    for (var t = 0; t < data_array.length; t++) {
-      team_info = data_array[t];
-      $teamselect.append('<option value="'+team_info.gsx$sheet.$t+'">'+team_info.gsx$teamname.$t+'</option>');
-    };
-    var $playerselect = $('<select/>', {id: 'players'+n, name: 'players'+n, multiple: 'true'}).append('<option class="loading" value="">Waiting for team selection...</option>');
+        .append('<option value="" selected="selected">Please select a team</option>');
+    // Possibly handle situation in which responses is not an array.
+    // But for now:
+    $.each(arguments, function(index, data){
+      var data_array = data[0].feed.entry;
+      var team_info = [];
+      // HACKY!  We know the feed.link[0].href is of the form https://spreadsheets.google.com/pub?key=0ApzvRgA17RKMdGJsaVZHTjZsb204bUJRQUtCcS1Id1E, so just take what's after the = sign.
+      var division_key = data[0].feed.link[0].href.split('=')[1];
+      $teamselect.append('<optgroup label="'+div_order[index]+'">');
+      for (var t = 0; t < data_array.length; t++) {
+        team_info = data_array[t];
+        $teamselect.append('<option value="'+division_key+','+team_info.gsx$sheetnumber.$t+'">'+team_info.gsx$teamname.$t+'</option>');
+      };
+      $teamselect.append('</optgroup>');
+    });
+    var $playerselect = $('<select/>', {id: 'players'+n, name: 'players'+n, multiple: 'true'})
+      .append('<option class="loading" value="">Waiting for team selection...</option>');
     $teamselect.change(function(){
       $playerselect.find('option').remove();
       $playerselect.append('<option class="loading" value="">Loading...</option>');
-      reg_get(reg_url+$teamselect.val()+format_suffix).done(function(data, textStatus, jqXHR){
+      // Know the value is passed of the form KEYSTRING,Sheet#.
+      var team_vals = $teamselect.val().split(',');
+      reg_get('http://spreadsheets.google.com/feeds/list/'+team_vals.join('/')+format_suffix).done(function(data, textStatus, jqXHR){
         $playerselect.find('option.loading').remove();
         var data_array = data.feed.entry;
         var player_info = [];
         for (var p = 0; p < data_array.length; p++){
-          console.log(data_array[p]);
           player_info = data_array[p];
           $playerselect.append(
             '<option value="'
@@ -213,9 +225,6 @@ function build_team_list(n) {
   });
   return $fieldset;
 };
-//function callbackfun(json){
-//  open_json = json;
-//}
   </script>
 <!--  <script src="http://spreadsheets.google.com/feeds/list/0ApzvRgA17RKMdFpyNUh0eWJKWVBuRmJlSkh5TGpNeWc/3/public/values?alt=json-in-script&amp;callback=callbackfun"></script>-->
 </body>
