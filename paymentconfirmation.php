815da851646acdf52edabbb3872cbdaa30e778b9
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2013-11-29 16:26:29 -1000

    Add payment confirmation page.

---
+++ paymentconfirmation.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html lang="en" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html lang="en" class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html lang="en" class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+  <?php require('partial/head.php'); ?>
+  <title>Payment Confirmation</title>
+  <meta name="description" content="Kaimana Klassik 27 player registration confirmation">
+  <meta name="keywords" content="Kaimana Klassik Oahu ultimate frisbee payment registration confirmation social media twitter facebook">
+</head>
+<body>
+  <?php include('partial/oldBrowser.php'); ?>
+  <div class="wrapper grid-parent">
+  <header>
+    <?php include('partial/banner.php'); ?>
+    <?php require('partial/nav.php'); ?>
+  </header>
+  <!-- Add your site or application content here -->
+  <div class="content">
+	<section class="grid-100">
+      <article>
+        <h1>Thank You</h1>
+        <p>
+          Thanks for your payment.
+        </p>
+      </article>
+	</section>
+<!--    <aside class="grid-30">
+      <p>
+        Sample Text.
+      </p>
+	</aside>-->
+  </div>
+  <?php include('partial/foot.php'); ?>
+  </div> <!-- /wrapper -->
+  <?php require('partial/scripts.php'); ?>
+</body>
+</html>
