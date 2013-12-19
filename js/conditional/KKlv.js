e83298bfb992db646c112d89295fcc14dc319f94
Clay McKell on Bellman <kcmckell@gmail.com>
2013-12-18 16:32:18 -1000

    Start working with LV JSON responses.
    Not currently reporting teams.  Work on implementing AJAX/deferred workflow.

--- js/conditional/KKlv.js
+++ js/conditional/KKlv.js
@@ -18,7 +18,38 @@ jQuery(document).ready(function(){
 			* json.objects[0,1]
 			* Each of these is a JSON tournament object.
 			*/
-			console.log(json);
+			console.log(json.objects);
+			var $target = $('#LV_list_teams'),
+				add_us = $();
+			for (var d=0; d<json.objects.length; d++) {
+				var thisdiv = json.objects[d];
+				var $d = $('<div/>',{'class':'grid-parent'});
+				$d.append('<h2><a href="'+thisdiv.leaguevine_url+'">'+thisdiv.name+'</a></h2>');
+				$.getJSON(LV_api_url+
+					'tournament_teams/?tournament_ids='+encodeURIComponent(JSON.stringify([thisdiv.id]))+
+					'&access_token='+LV_access_token,
+					function(teams_json){
+						for (var t=0; t<teams_json.objects.length; t++){
+							var thisteam = teams_json.objects[t];
+							var $t = $('<div/>',{'class':'team_row'});
+							$t.append([
+								$('<span/>',{
+									'class':'grid-10',
+									'text':thisteam.seed
+								}),
+								$('<span/>',{
+									'class':'grid-90',
+									'text':'<a href="'+thisteam.team.leaguevine_url+'">'+thisteam.team.name+'</a>'
+								})
+							]);
+						}
+					});
+				
+				//Write to document
+				$target.append($d);
+			}
+			
+
 		}
 	)
 });
