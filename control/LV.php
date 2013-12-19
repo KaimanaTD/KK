b3194763ff9a2b1acdffe4601cc6909de09b1fa9
Clay McKell on Big Baby <kcmckell@gmail.com>
2013-12-19 13:29:57 -1000

    Fix LV.php to handle multiple query params.

--- control/LV.php
+++ control/LV.php
@@ -2,13 +2,18 @@
 define('SECURE_CONSTANT_173945d5ecd6224993ffe110dfb30fa0',1);
 date_default_timezone_set('Pacific/Honolulu');
 require_once 'LV_settings.php';
-$out = array();
-if (isset($_GET['tournament_id'])) {
-  array_push($out,"Found set key.");
-  define('LV_TOURNAMENT_ID',$_GET['tournament_id']);
-  $curl = curl_init();
-  curl_setopt_array($curl, array(
-      CURLOPT_URL => KK_LEAGUEVINE_API_URL . 'tournaments/' . LV_TOURNAMENT_ID . '/?access_token=' . KK_LEAGUEVINE_ACCESS_TOKEN,
+$single_params = array(
+    'tournament_id' => 'tournaments/',
+    'tournament_team' => 'tournament_teams/',
+    'team_id' => 'teams/'
+);
+$multiple_params = array(
+    'tournaments' => 'tournaments/?tournament_ids=',
+    'tournament_teams' => 'tournament_teams/?tournament_ids=',
+    'teams' => 'teams/?team_ids='    
+);
+$curl = curl_init();
+curl_setopt_array($curl, array(
       CURLOPT_RETURNTRANSFER => 1,
       CURLOPT_HTTPHEADER => array(
           'Content-type' => 'application/x-www-form-urlencoded',
@@ -16,11 +21,23 @@ if (isset($_GET['tournament_id'])) {
           'Authorization' => 'bearer ' . KK_LEAGUEVINE_ACCESS_TOKEN 
       )
   ));
-  $result = curl_exec($curl);
-  curl_close($curl);
-  array_push($out, $result, curl_getinfo($curl), KK_LEAGUEVINE_API_URL . 'tournaments/' . LV_TOURNAMENT_ID . '/?access_token=' . KK_LEAGUEVINE_ACCESS_TOKEN);
-} else {
-  array_push($out, "Nothing Found.");
+foreach ($multiple_params as $k => $v) {
+  if (isset($_GET[$k])) {
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => KK_LEAGUEVINE_API_URL . $v . urlencode($_GET[$k]) . '&access_token=' . KK_LEAGUEVINE_ACCESS_TOKEN
+    ));
+    break;
+  }
 }
+foreach ($single_params as $k => $v) {
+  if (isset($_GET[$k])) {
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => KK_LEAGUEVINE_API_URL . $v . $_GET[$k] . '/?access_token=' . KK_LEAGUEVINE_ACCESS_TOKEN
+    ));
+    break;
+  }
+}
+$result = curl_exec($curl);
+curl_close($curl);
 echo $result;
 ?>
