64b44639fe63b038db29a98153921872270007de
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2014-01-06 11:32:28 -0800

    Start work on results and schedule page.
    Page called "results.php" will hold the schedule (and then results) of all games.  Set up chronologically, with early games on top and later games on the bottom.
    Game data is caled via Leaguevine API.
    Modernizr will only call this page's scripts on this page.

---
+++ js/conditional/LV-results.js
@@ -0,0 +1,58 @@
+// URL for LV women: https://www.leaguevine.com/tournaments/19435/kaimana-klassik-women/
+var ID_women = 19435;
+// URL for LV open: https://www.leaguevine.com/tournaments/19436/kaimana-klassik-open/
+var ID_open = 19436;
+var no_content = "This information is not currently available.";
+
+jQuery(document).ready(function() {
+  $.getScript('js/conditional/LV-utils.js', function(){
+    //
+    // Display Pools
+    //
+    var trunk = 'pools';
+    var o_query = 'tournament_id=' + JSON.stringify(ID_open);
+    var w_query = 'tournament_id=' + JSON.stringify(ID_women);
+    get_LV({'trunk':trunk,'query':o_query}).done(render_pools);
+    get_LV({'trunk':trunk,'query':w_query}).done(render_pools);
+  })
+})
+
+function render_pools(data,Textstatus,jqXHR){
+  console.log(data);
+  var gender = -1;
+  if (data["0"].indexOf(ID_women)>0) {
+    gender = 0;
+  } else if (data["0"].indexOf(ID_open)>0) {
+    gender = 1;
+  }
+  if (gender == -1) {console.log("Could not determine division."); return null;}
+  var content = [$('<h3>'+(gender ? 'Open' : 'Womens')+' Pools</h3>')];
+  var $poolwrapper,
+    $poolname,
+    $poolgame,
+    pool;
+  if (data.objects.length>0) {
+    $poolwrapper = $('<div/>',{
+      'class':'pool_wrapper'
+    });
+    for (var p=0; p<data.objects.length; p++) {
+      /*
+       * TODO:
+       * Get game info for every team in the pool.  How?
+       */
+      pool = data.objects[p];
+      $poolname = $('<div/>',{
+        'class':'grid-100 pool_name'
+      }).html('<a href="'+pool.leaguvine_url+'">Pool '+pool.name+'</a>');
+      $poolname.append($('<div/>'),{
+        'class':'pool_fields',
+        'text':'' // TODO Get field info for the pool here.
+      });
+      
+    }
+  } else {
+    content.push($('<p>'+no_content+'</p>'));
+  }
+  var target = (gender ? $('#LV_sat_pools_open') : $('#LV_sat_pools_women'));
+  target.append(content);
+};
---
+++ results.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html lang="en" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html lang="en" class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html lang="en" class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+  <?php require('partial/head.php'); ?>
+  <title></title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+</head>
+<body>
+  <?php include('partial/oldBrowser.php'); ?>
+  <div class="wrapper grid-parent">
+  <header>
+    <?php include('partial/banner.php'); ?>
+    <?php require('partial/nav.php'); ?>
+  </header>
+  <!-- Add your site or application content here -->
+  <div class="content">
+	<section id="results">
+      <article class="grid-100">
+        <h1>Schedule &amp; Results</h1>
+        <p>
+          For a quicker connection, you may want to see the <a href="https://www.leaguevine.com/tournaments/19436/kaimana-klassik-open/">Open</a> and <a href="https://www.leaguevine.com/tournaments/19435/kaimana-klassik-women/">Women</a> results on Leaguevine.
+        </p>
+      </article>
+      <article class="grid-100 grid-parent">
+        <h2>Saturday Pools</h2>
+        <div id="LV_sat_pools_open" class="grid-50 mobile-grid-100 grid-parent"></div>
+        <div id="LV_sat_pools_women" class="grid-50 mobile-grid-100 grid-parent"></div>
+      </article>
+	</section>
+    <?php /*
+    <aside class="grid-30">
+      <p>
+        Sample Text.
+      </p>
+	</aside>
+     */ ?>
+  </div>
+  <?php include('partial/foot.php'); ?>
+  </div> <!-- /wrapper -->
+  <?php require('partial/scripts.php'); ?>
+</body>
+</html>
