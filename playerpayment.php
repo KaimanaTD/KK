fba717cb8d6774df486b22c5b4153d5e32ba74c6
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2014-01-08 23:05:17 -1000

    Improve countdown timer.
    Now differentiates between seconds and minutes remaining.

--- playerpayment.php
+++ playerpayment.php
@@ -107,23 +107,30 @@
     //deadline = new Date(Date.parse(now)+1000*30);
     console.log('Now:'); console.log(now);
     console.log('Deadline:'); console.log(deadline);
-    if (deadline - now < 5*60*1000) {
-      displayTimeWarning(deadline-now);
-      var interval = setInterval(clocktick, 1000);
-    } 
-//	var latewarningDOM = document.getElementById("deadline_warning");
-    function clocktick() {
-		now = Date(Date.parse(now)+1000);
-		var d = new Date(now);
-        console.log("We are "+(d>deadline?"after":"before")+" the deadline.");
-		if (d > deadline) {
-          reloadWithinTime(d-deadline);
-			prices['player'] = prices.player_late;
-            $('p#time_warning').remove();
-		} else {
-          $('span#increase_time').html((deadline-d)/1000);
-        };
-	};
+    if (deadline - now < 5*60*1000 && deadline - now > -2000) {
+    displayTimeWarning(deadline-now);
+    var interval = setInterval(clocktick, 1000);
+  } 
+  //	var latewarningDOM = document.getElementById("deadline_warning");
+  function clocktick() {
+    now = Date(Date.parse(now)+1000);
+    var d = new Date(now);
+    var s = (deadline-d)/1000;
+    console.log("We are "+(d>deadline?"after":"before")+" the deadline.");
+    if (d > deadline) {
+      reloadWithinTime(deadline-d);
+      prices['player'] = prices.player_late;
+      $('p#time_warning').remove();
+    } else {
+      if (s > 60 ) {
+        $('span#increase_time').html(Math.floor(s/60));
+        $('span#increase_time_units').html('minute'+(Math.floor(s/60)==1 ? '' : 's'));
+      } else {
+        $('span#increase_time').html(s);
+        $('span#increase_time_units').html('seconds');
+      }
+    };
+  };
     var PAYPAL_ITEM_MAXLEN = 127;
     // Registration fanciness
 	var $PPbutton = $('div#reg_pay');
@@ -331,7 +338,19 @@ function build_team_list(n) {
 
 function displayTimeWarning(t){
   var $target = $('#payment_instructions');
-  $target.append('<p id="time_warning">WARNING: Fee Increase occurs in <span id="increase_time">'+t/1000+'</span> seconds.</p>');
+  $target.append(
+    '<p id="time_warning">'+
+      'WARNING: Fee increase occurs in '+
+      '<span id="increase_time">'+
+        ( t<1000*60 ? t/1000 : Math.floor(t/1000/60) )+
+      '</span>'+
+      ' '+
+      '<span id="increase_time_units">'+
+        ( t<1000*60 ? 'seconds' : 'minute'+( Math.floor(t/1000/60)==1 ? '' : 's' ) )+
+      '</span>.  '+
+      'This will result in the page being reloaded, and you will have to start the payment process over.'+
+    '</p>'
+  );
 };
 
 function reloadWithinTime(t){
