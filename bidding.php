fb944e4808901838e0fcf46bb82e84e9e6171695
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2014-10-02 23:27:00 -0700

    Update bidding.php to KK28.
    Remove some dates we are not sure of yet.
    Update the dates we know (via the centralized `$date`.

--- bidding.php
+++ bidding.php
@@ -4,8 +4,11 @@
 <!--[if IE 8]>         <html lang="en" class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
 <head>
-  <?php require('partial/head.php'); ?>
-  <title>KK27 | Bids & Fees</title>
+  <?php 
+    require('partial/head.php');
+    define('SECURE_CONSTANT_173945d5ecd6224993ffc110dfb30fa0',1);
+	require_once('control/dates.php');
+  ?>
   <meta name="description" content="Instructions for bids and fees for Kaimana Klassik.">
   <meta name="keywords" content="Kaimana Klassik ultimate Hawaii frisbee Waimanalo Oahu open women disc bids bidding fees payment">
 </head>
@@ -28,6 +31,7 @@
         <p>
           All players should <a href="playerregistration.php">register</a> and <a href="playerpayment.php">pay online</a>, if possible.  
           Individual online registration and payment will be open once team bids have been accepted (mid to late November).  
+<!--
           If you cannot pay online, please do so by sending a check made out to <strong>Kaimana Klassik</strong> to: 
 <blockquote>Jack Wade 
 1308 Center Street 
@@ -59,6 +63,7 @@ Honolulu, HI 96816
           No one will be allowed to enter the party without a player "sponsor".
           </span>
         </p>
+-->
 		</article>
 		<article>
           <h2>How to submit an individual bid</h2>
@@ -76,8 +81,8 @@ Honolulu, HI 96816
           </p>
           <ul>
             <li>The $420 initial deposit can later be applied to three individual player's fees (the captain plus two other players). For those teams that do not make the final cut, we will refund your full deposit.</li>
-            <li>Bid-winning teams will be notified via email (as will wait-listed teams) by November 15. Bid-winning teams must respond via e-mail or letter </li>
-            by November 22, whether or not they wish to accept the bid. If no response is received, that team will forfeit its bid, and the next wait-listed team will be contacted. At this point, the declining bid-winning team will receive a 100% refund on its deposit.</li>
+            <li>Bid-winning teams will be notified via email (as will wait-listed teams) by <?php echo date('F j',$date['bid_notification']);?>. 
+              Bid-winning teams must respond via e-mail or letter by November 22, whether or not they wish to accept the bid. If no response is received, that team will forfeit its bid, and the next wait-listed team will be contacted. At this point, the declining bid-winning team will receive a 100% refund on its deposit.</li>
             <li>Bid-winning teams are required to secure their bid by submitting entrance fees for at least 8 players (at $140 per player) by December 13.</li>
             <li>If a bid-winning and bid-accepting team fails to submit the above mentioned players' fees, its bid will be lost, and the next wait-listed team will be given priority. If we are able to fill this new vacancy, we will issue only a partial deposit refund of $160. If we are unable to fill the vacancy, no refund of deposit will be issued.</li>
             <li>The remainders of each team's entrance fees are due by January 15. Any entrance fee not paid in full by this date will be assessed the late registration fee of $165.</li>
@@ -88,11 +93,11 @@ Honolulu, HI 96816
 		<aside class="grid-30">
           <h2>Announcements</h2>
           <dl>
-            <dt>December 2, 2013</dt><dd><a href="playerregistration.php">Player and guest registration</a> is now open.</dd>
-            <dt>September 19, 2013</dt><dd><a href="onlinebidform.php">Team bids</a> are now being accepted.</dd>
+            <!--<dt>December 2, 2013</dt><dd><a href="playerregistration.php">Player and guest registration</a> is now open.</dd>-->
+            <dt>October 2, 2014</dt><dd><a href="onlinebidform.php">Team bids</a> are now being accepted.</dd>
           </dl>
-          <h2>Important Dates</h2>
-          <dl>
+          <h2 class="hidden">Important Dates</h2>
+          <dl class="hidden">
                   <dt>November 8, 2013</dt><dd> Deadline for team bids (we must receive info and payment by then).</dd>     
                   <dt>November 15, 2013</dt><dd> Accepted, wait-listed, and declined teams will be notified.</dd>    
                   <dt>November 22, 2013</dt><dd> Teams must accept/decline bids.</dd>    
