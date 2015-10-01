516674fa6d78ce8c5a60f7e972717fb7a8d91d7e
KaimanaTD <KaimanaTD@gmail.com>
2015-10-01 13:15:35 -1000

    Remove paypal button, add ulitcentral link

--- onlinepayment.php
+++ onlinepayment.php
@@ -28,18 +28,7 @@
 		<p id="deadline_warning" class="hidden">
 			<strong>NOTICE: Team bid payments submitted after midnight Hawaii Standard Time on <?php echo date('M j, Y',$date['bids_close']); ?>  will be considered late.</strong>
 		</p>
-        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-          <input type="hidden" name="cmd" value="_s-xclick">
-          <input type="hidden" name="hosted_button_id" value="S6CZGX43UQEBW">
-          <table>
-            <tr><td><input type="hidden" name="on0" value="Kaimana Klassik 28 Team Deposit">Kaimana Klassik 28 Team Deposit</td></tr><tr><td><select name="os0">
-                <option value="Team Deposit">Team Deposit $420.00 USD</option>
-            </select> </td></tr>
-          </table>
-          <input type="hidden" name="currency_code" value="USD">
-          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-          <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-        </form>
+        <li>Team Bid Registration can be found here http://ultimatecentral.com/en_US/e/kaimana-klassik-29</li>
       </article>
 	</section>
   </div>
