c9689de356ccb3d0b497f0ef6e510c96a0297d5e
KaimanaTD <KaimanaTD@gmail.com>
2016-06-25 08:03:09 -1000

    Update banner.php

--- partial/banner.php
+++ partial/banner.php
@@ -1,3 +1,3 @@
 <div class="banner">
-  <h1 class="ir">Kaimana Klassik 29: February 13-15, 2016.  Waimanalo, HI.</h1>
+  <h1 class="ir">Kaimana Klassik 30: February 18-20, 2017.  Waimanalo, HI.</h1>
 </div>
